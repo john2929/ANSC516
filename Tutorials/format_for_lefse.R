a957dd1b31c0cdf42a8b913f62b8a84d3fc0d20d
Tim Johnson <john2185@purdue.edu>
2022-04-14 09:26:27 -0400

    Update format_for_lefse.R
    Added in comments to help with understanding the code.

--- Tutorials/format_for_lefse.R
+++ Tutorials/format_for_lefse.R
@@ -8,26 +8,34 @@ metadata<-read_q2metadata("sample-metadata.tsv")
 str(metadata)
 colnames(metadata)[3] = "bodysite"
 levels(metadata$bodysite)
+
+## Select and keep the columns you need to use as:
+##    class, subclass (optional) and subject
 metadata_filtered <- metadata %>%
   select(SampleID, bodysite)
 str(metadata_filtered)
 
+# Read in ouput from qiime2
 taxonomy <- read.delim("collapse.frequency.table.txt", skip = 1)
 head(taxonomy)
+
+# Some reformatting
 colnames(taxonomy)[1] <- "Taxon"
 taxonomy <- taxonomy[,-(ncol(taxonomy))]
 taxonomy$Feature.ID <- paste0("ASV", 1:nrow(taxonomy))
-#taxonomy <- taxonomy[,-7]
 
+# split the taxonomy into one level per column and remove the "K__" stuff
 tax.clean<-parse_taxonomy(taxonomy = taxonomy, tax_sep = ";")
 tax.clean <- tax.clean[,-7]
 head(tax.clean)
-tax.clean[tax.clean=="__"] <- ""
 
+# Make the unclassified cells be empty so that we can edit them 
+tax.clean[tax.clean=="__"] <- ""
 tax.clean[is.na(tax.clean)] <- ""
 
 ncol(tax.clean) ##This should equal 6. If not the code will need to be modified.
 
+# Change empty cells to say "unclassified" with the last assigned taxonomic level
 for (i in 1:nrow(tax.clean)){
   if (tax.clean[i,2] == ""){
     kingdom <- paste("unclassified_", tax.clean[i,1], sep = "")
@@ -47,14 +55,26 @@ for (i in 1:nrow(tax.clean)){
   } 
 }
 
+# Now finish up formatting into the format required by lefse
+
+# Get taxonomy into one cell separated by "|"
 tax.clean_lefse <- tax.clean %>%
   unite("lefse", Kingdom:Genus, sep= "|", 
         remove = FALSE)
 
+# Merge taxonomy with relative abundance for each sample
 tax.clean_lefse <- merge(tax.clean_lefse, taxonomy, by.x = 0, by.y = "Feature.ID")
 tax.clean_lefse <- tax.clean_lefse[,-3:-9]
+
+
+#Merge metadata with lefse table. Have to transpose the table to do this.
 t.tax <- t(tax.clean_lefse)
 meta_lefse <- merge(metadata_filtered, t.tax, by.x = "SampleID", by.y = 0, all = T)
+
+#remove spaces in bodysite names in the bodysite column
+meta_lefse$bodysite <- as.data.frame(gsub(" ", "_", meta_lefse$bodysite))
+
+# final formatting to make it pretty
 t.meta_lefse <- as.data.frame(t(meta_lefse))
 colnames(t.meta_lefse) <- t.meta_lefse[1,]
 t.meta_lefse <- t.meta_lefse[-1,]
@@ -62,9 +82,12 @@ final_lefse <- t.meta_lefse %>% select(Row.names, lefse, everything())
 final_lefse <- final_lefse[,-1]
 colnames(final_lefse)[1] <- "SampleID"
 final_lefse[1,1] <- "bodysite"
+
+# Save the table `final_lefse` onto your computer
 write.csv(final_lefse,"lefse_taxonomy.csv", row.names = F) 
 
-#You will need to do some manual reformatting to make this work:
-#Make bodysite the first row, SampleID the second row.
-#No cell can have spaces. Search for spaces and replace with underscore
-#DON'T FORGET: Save as a tab-separated value file!!!
+# Final Notes:
+# You will need to do some manual reformatting to make this work:
+# Make bodysite the first row, SampleID the second row.
+# No cell can have spaces. Search for spaces and replace with underscore if needed
+# DON'T FORGET: Save as a tab-separated value file in MS excel!!!
